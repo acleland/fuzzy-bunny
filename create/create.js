845c540ab1ae2420c1aa6ba6dfd23cb690130492
Andrea Cleland <andreaclelan@gmail.com>
2022-02-22 12:06:59 -0800

    implemented the on-load event listener that gives user the list of families to choose from

--- create/create.js
+++ create/create.js
@@ -7,30 +7,27 @@ import {
 
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
+const selectFamily = document.getElementById('select-family');
 
-form.addEventListener('submit', async e => {
+form.addEventListener('submit', async (e) => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
+    const formData = new FormData(form);
 
     // use createBunny to create a bunny with this name and family id
     
     form.reset();
 });
 
-window.addEventListener('load', async() => {
-    // let's dynamically fill in the families dropdown from supabase
-    // grab the select HTML element from the DOM
-
-    // go get the families from supabase
-
-    // for each family
-
-    // create an option tag
-
-    // set the option's value and text content
-
-    // and append the option to the select
+window.addEventListener('load', async () => {
+    const families = await getFamilies();
+    for (let family of families) {
+        const option = document.createElement('option');
+        option.value = family.id;
+        option.textContent = family.name;
+        selectFamily.append(option);
+    }
 });
 
 
